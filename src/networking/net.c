029e5d9ad27ba1f4d39b8886ee61c71dad174035
EthanSk13s <ethanmiranda11@gmail.com>
2025-08-04 19:46:22 -0700

    add buffer writing callback

--- src/networking/net.c
+++ src/networking/net.c
@@ -1,9 +1,34 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "net.h"
 
-#define MAX_BUFFER_SIZE 5000
+#define INITIAL_BUFFER_SIZE 5000
+
+static size_t write_callback(char* data, size_t size, size_t nmemb, void* user_data)
+{
+    size_t realsize = size * nmemb;
+    Buffer* buffer = (Buffer*) user_data;
+
+    if (buffer->max_length < buffer->buf_length + realsize)
+    {
+        char* new_buf = realloc(buffer->buffer, buffer->buf_length + realsize + 1);
+        if (!new_buf)
+        {
+            return 0;
+        }
+
+        buffer->buffer = new_buf;
+        buffer->max_length = buffer->buf_length + realsize + 1;
+    }
+
+    memcpy(&buffer->buffer[buffer->buf_length], data, realsize);
+    buffer->buf_length += realsize;
+    buffer->buffer[buffer->buf_length] = 0;
+
+    return realsize;
+}
 
 int net_init_handler(NetworkHandler* net_handler, char* url)
 {
@@ -20,17 +45,20 @@ int net_init_handler(NetworkHandler* net_handler, char* url)
     }
 
 
-    net_handler->buf.buffer = malloc(MAX_BUFFER_SIZE);
+    net_handler->buf.buffer = malloc(INITIAL_BUFFER_SIZE);
     if (!net_handler->buf.buffer)
     {
         return -1;
     }
 
-    net_handler->buf.max_length = MAX_BUFFER_SIZE;
+    net_handler->buf.max_length = INITIAL_BUFFER_SIZE;
     net_handler->buf.buf_length = 0;
 
     strncpy(net_handler->url, url, NET_MAX_URL_LENGTH);
+
     curl_easy_setopt(net_handler->curl, CURLOPT_URL, net_handler->url);
+    curl_easy_setopt(net_handler->curl, CURLOPT_WRITEDATA, (void*) &net_handler->buf);
+    curl_easy_setopt(net_handler->curl, CURLOPT_WRITEFUNCTION, write_callback);
 
     return 0;
 }
