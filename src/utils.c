3c45298466b4f1511fa2f1685d5a336825f80b94
EthanSk13s <ethanmiranda11@gmail.com>
2025-08-20 19:02:34 -0700

    use correct parameter

--- src/utils.c
+++ src/utils.c
@@ -7,7 +7,7 @@ int compare_color(Color x, Color y)
 
 Vector2 utils_center_text(Font font, Vector2 origin, char* text, int font_size, int spacing)
 {
-    Vector2 center_text = MeasureTextEx(font, text, 20, 2);
+    Vector2 center_text = MeasureTextEx(font, text, font_size, 2);
     Vector2 center = { ((float) origin.x / 2) - (center_text.x / 2),
                        ((float) origin.y / 2) - (center_text.y / 2)
                      };
