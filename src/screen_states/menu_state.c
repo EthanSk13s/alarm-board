2da098ca2cd6b9a5c080ea9e7574ae793fbb999f
EthanSk13s <ethanmiranda11@gmail.com>
2025-11-09 17:47:00 -0800

    add menu screen

---
+++ src/screen_states/menu_state.c
@@ -0,0 +1,94 @@
+#include <stdlib.h>
+
+#include "menu_state.h"
+#include "clock_state.h"
+#include "weather_state.h"
+
+#include "../info_storage.h"
+#include "../managers/sprite_man.h"
+#include "../managers/ui_man.h"
+
+struct MenuData
+{
+    int textures_loaded;
+    SpriteManager* sprite_manager;
+    UIManager ui_manager;
+};
+
+static struct MenuData menu_data;
+static void clock_btn_callback(void* state);
+static void wthr_btn_callback(void* state);
+
+void menu_update(ScreenStatePtr state)
+{
+    ui_man_poll(&menu_data.ui_manager);
+}
+
+void menu_draw(ScreenStatePtr state)
+{
+    ClearBackground(BLACK);
+    draw_sprites(menu_data.sprite_manager);
+}
+
+void transition_to_menu(ScreenStatePtr state)
+{
+    default_state(state);
+
+    state->update = menu_update;
+    state->draw = menu_draw;
+
+    int width = get_current_width();
+    int height = get_current_height();
+
+    if (!menu_data.textures_loaded)
+    {
+        menu_data.sprite_manager = create_sprite_manager();
+        ui_man_init(&menu_data.ui_manager);
+
+        Texture2D* wthr_texture = texture_manager_get(get_texture_man(), "day_clouded");
+        Texture2D* clock_texture = texture_manager_get(get_texture_man(), "clock");
+
+        TextureSet btn_texture_opts = { 10 , 0 };
+        // TODO: Create this button bar separately. 80 is from total bar width from
+        // combining textures.
+        int btn_bar_x = (width / 2) - 160;
+        int btn_bar_y = height / 2; 
+        Sprite* clock_sprite = create_sprite(btn_bar_x,
+                                             btn_bar_y - 80,
+                                             clock_texture,
+                                             btn_texture_opts,
+                                             WHITE,
+                                             0);
+        Sprite* wthr_sprite = create_sprite(btn_bar_x + 160,
+                                            btn_bar_y - 80,
+                                            wthr_texture,
+                                            btn_texture_opts,
+                                            WHITE,
+                                            0);
+
+        add_to_sprite_manager(menu_data.sprite_manager, wthr_sprite);
+        add_to_sprite_manager(menu_data.sprite_manager, clock_sprite);
+
+
+        Button* clk_btn = malloc(sizeof(Button));
+        Button* wthr_btn = malloc(sizeof(Button));
+
+        btn_init(clk_btn, clock_sprite, clock_btn_callback, state);
+        btn_init(wthr_btn, wthr_sprite, wthr_btn_callback, state);
+
+        ui_man_add(&menu_data.ui_manager, clk_btn);
+        ui_man_add(&menu_data.ui_manager, wthr_btn);
+
+        menu_data.textures_loaded = 1;
+    }
+}
+
+static void clock_btn_callback(void* state)
+{
+    transition_to_clock(state);
+}
+
+static void wthr_btn_callback(void* state)
+{
+    transition_to_wthr_state(state);
+}
---
+++ src/screen_states/menu_state.h
@@ -0,0 +1,9 @@
+#ifndef MENU_STATE_H
+#define MENU_STATE_H
+
+#include "screen_state.h"
+
+void transition_to_menu(ScreenStatePtr state);
+void clean_up_menu_state();
+
+#endif
